07f857f81c742a9ee50f09645c1d050c829e4ef0
NEWANDNEWIDE <yaoguantingpro@gmail.com>
2024-11-07 10:42:10 +0800

    test

--- game_master/item.py
+++ game_master/item.py
@@ -5,10 +5,10 @@ import game_master.fileManager
 class Item:
     def __init__(self):
         self.__bgp = []
-        self.button_list = [0]
-        self.surface_button_list = [0]
-        self.box_list = [0]
-        self.surface_box_list = [0]
+        self.button_list = [1]
+        self.surface_button_list = [1]
+        self.box_list = [1]
+        self.surface_box_list = [1]
         self.button = control.button.Button()
         self.surfaceButton = control.button.SurfaceButton()
         self.textBox = control.textBox.TextBox()
@@ -17,3 +17,13 @@ class Item:
 
     def load(self):
         item = game_master.fileManager.loading_item()
+
+
+class Bag:
+    def __init__(self):
+        self.__bag = [-1 for _ in range(36)]
+        self.__state = False
+        self.__surface = game_master.fileManager.game_surface["bag"]
+
+    def insert(self, pos, data):
+        self.__bag[pos] = data
